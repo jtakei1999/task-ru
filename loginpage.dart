1e950271a2dab2a26675225f015fced345708e2c
natsuko1225 <93573866+natsuko1225@users.noreply.github.com>
2021-11-07 11:09:19 +0900

    Add files via upload
    シーン遷移できた！！！

--- loginpage.dart
+++ loginpage.dart
@@ -1,74 +1,75 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title:const Text('ログイン'),
-        ),
-        body: SafeArea(
-    child: LayoutBuilder(
-    builder: (context, constraints)
-    {
-      return SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(minHeight: constraints.maxHeight),
-          child: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextField(
-                      controller: null,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
+      appBar: AppBar(
+        title: const Text('ログイン'),
+      ),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextField(
+                            controller: null,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              labelText: 'ID',
+                            ),
+                          ),
+                          const SizedBox(height: 24.0),
+                          TextField(
+                            controller: null, // Controller実装必要
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              labelText: 'パスワード',
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 54.0,
+                        decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(8.0),
                         ),
-                        labelText: 'mail address',
-                      ),
-                    ),
-                    const SizedBox(height: 24.0),
-                    TextField(
-                      controller: null, // Controller実装必要
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blue),
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.white),
+                          ),
+                          onPressed: () {
+                            // ボタン処理
+                          },
+                          child: const Text('ログイン'),
                         ),
-                        labelText: 'password',
                       ),
-                    ),
-                  ],
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 54.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: TextButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                      MaterialStateProperty.all(Colors.blue),
-                      foregroundColor:
-                      MaterialStateProperty.all(Colors.white),
-                    ),
-                    onPressed: () {
-                      // ボタン処理
-                    },
-                    child: const Text('Log in'),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
-      );
-    },
-    ),
-    ),
+      ),
     );
-}
   }
+}
--- main.dart
+++ main.dart
@@ -2,46 +2,58 @@ import 'package:flutter/material.dart';
 
 import 'loginpage.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      //theme: ThemeData(primarySwatch: Colors.blue,
-        home: Scaffold(
-          appBar: AppBar(
-            title:const Text('Hello!'),
-          ),
-      body:Center(
-      child:SafeArea(child:Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children:[Image.network('https://lh3.googleusercontent.com/_QKN2QGGDG8tC_2Vg6XUrf0gS6SUP9jN9NSa9yZZYKCr6qvjuWSro-GPyYpzBA0OMk2Vu9KBPBWM8lFXSAC6-K_q_SqoPOuYJtS5TCgZUxUrnlZWF_RosLPeFZlRLvHPKQ=w1280'),
-             Container(
-               padding: EdgeInsets.only(top: 40),
-               child: Text('早速始めよう！', style: TextStyle(fontSize: 25)),
-             ),
-             Row(
-               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-               children: <Widget>
-               [
-                 ElevatedButton(
-                   onPressed: () { Navigator.push(
-                       context, MaterialPageRoute(builder: (context) => LoginPage()));},
-                   child: Text('ログイン',style: TextStyle(fontSize: 22)),
-                 ),
-                 ElevatedButton(
-                     onPressed: () { /* ボタンがタップされた時の処理 */ },
-                     child: Text('新規会員登録',style: TextStyle(fontSize: 20))
-                 ),
-               ],
-             ),
-           ],
-         ),)
-      ),),
+      home: Home(),
     );
   }
+}
+
+class Home extends StatelessWidget {
+  @override
+  // ここのcontextは上位ツリーで提供されたMaterialAppを含んでいる。
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello!'),
+      ),
+      body: Center(
+          child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+                'https://lh3.googleusercontent.com/lvmh98RIVyztPDlJ_jl_gYASIV46lrwqL4a9D-j74nWymCe5oDUBrr-9xHpH31zwNVilbRFfYguRl-WiGG68zqOI6Y_TLBJyrQPInoXJngDOPKQw2dJHyzhgBsiplOIhEQ=w1280'),
+            Container(
+              padding: EdgeInsets.only(top: 40),
+              child: Text('早速始めよう！', style: TextStyle(fontSize: 25)),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Text('ログイン', style: TextStyle(fontSize: 22)),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      /* ボタンがタップされた時の処理 */
+                    },
+                    child: Text('新規会員登録', style: TextStyle(fontSize: 20))),
+              ],
+            ),
+          ],
+        ),
+      )),
+    );
   }
+}
